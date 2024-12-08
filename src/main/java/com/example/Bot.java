34d49812546ed4e436261d52b5d680fbd90ee8c8
Denis Mukhametshin <dephigames@gmail.com>
2024-12-09 03:33:30 +0500

    Added '/start' and '/help' commands

--- src/main/java/com/example/Bot.java
+++ src/main/java/com/example/Bot.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @SuppressWarnings("deprecation")
@@ -8,16 +9,66 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return System.getenv("BOT_NAME");
+        return "BOT_NAME";
     }
 
     @Override
     public String getBotToken() {
-        return System.getenv("BOT_TOKEN");
+        return "BOT_TOKEN";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
 
+            switch (messageText) {
+                case "/start":
+                    sendStartMessage(chatId);
+                    break;
+
+                case "/help":
+                    sendHelpMessage(chatId);
+                    break;
+            }
+        }
+    }
+
+    private void sendStartMessage(Long chatId) {
+        String welcomeText = "Привет! Я ваш помощник по созданию общих рабочих пространств. С помощью меня вы можете:\r\n" + //
+                        "- Создавать и управлять общими сообщениями\r\n" + //
+                        "- Формировать To-Do списки для командной работы\r\n" + //
+                        "- Делиться заметками и идеями\r\n" + //
+                        "\r\n" + //
+                        "Используйте команду /help, чтобы узнать больше о доступных командах!\r\n" + //
+                        "";
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(welcomeText);
+        executeWithExceptionHandling(message);
+    }
+
+    private void sendHelpMessage(Long chatId) {
+        String helpText = "Вот список доступных команд:\r\n" + //
+                        "- /create_message [текст] - Создать общее сообщение для вашей команды.\r\n" + //
+                        "- /create_todo [задача] - Добавить задачу в To-Do список.\r\n" + //
+                        "- /view_todos - Просмотреть текущий To-Do список.\r\n" + //
+                        "- /delete_todo [номер задачи] - Удалить задачу из To-Do списка.\r\n" + //
+                        "\r\n" + //
+                        "Если у вас есть вопросы или нужна помощь, вы можете написать создателю бота!\r\n" + //
+                        "";
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(helpText);
+        executeWithExceptionHandling(message);
+    }
+
+    private void executeWithExceptionHandling(SendMessage message) {
+        try {
+            execute(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
